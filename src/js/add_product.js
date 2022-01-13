5c119b6397e306f2897c543d926009a8c8baaf44
choyeon <rlachdus2@naver.com>
2022-01-13 19:51:50 +0900

    ✨ 상품 등록 유효성검사 기능 추가

--- src/js/add_product.js
+++ src/js/add_product.js
@@ -18,3 +18,47 @@ imgInput.addEventListener('change', (e) => {
     
 })
 
+//유효성 검사 
+const itemName = document.getElementById('item-name');
+const itemPrice = document.getElementById('item-price');
+const itemLink = document.getElementById('item-link');
+const btnSave = document.querySelector('.btn-save');
+let nameFlag = false;
+let priceFlag = false;
+let linkFlag = false;
+
+itemName.addEventListener('keyup', function() {
+    const RegExp1 = /^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]{2,15}$/;
+    if (!RegExp1.test(itemName.value)) {
+        itemName.focus();
+        nameFlag = false;
+    } else {
+        nameFlag = true;
+    }
+    btnAttrChange()
+});
+
+itemPrice.addEventListener('keyup', function() {
+    const RegExp2 = /[^0-9]/gi;
+    itemPrice.value = itemPrice.value.replace(RegExp2,'');
+    btnAttrChange()
+});
+
+itemLink.addEventListener('keyup', function() {
+    const RegExp3 = /(http(s)?:\/\/)([a-z0-9\w]+\.*)+[a-z0-9]{2,4}/gi;
+    if(!RegExp3.test(itemLink.value)) {
+        let linkFlag = false;
+    } else {
+        linkFlag = true;
+    }
+    btnAttrChange()
+})
+
+// 저장 버튼 활성화 및 비활성화
+function btnAttrChange() {
+    if (nameFlag && itemPrice.value && linkFlag) {
+        btnSave.setAttribute('type', 'submit');
+    } else {
+        btnSave.setAttribute('type', 'button');
+    }
+}
