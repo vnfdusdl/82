559480795e1ae9b8fd85580d8415407e1de3b280
choyeon <rlachdus2@naver.com>
2022-01-13 16:56:51 +0900

    ⚡️ input 유효성검사 개선 input에 입력 시 실시간으로 유효성검사가 일어나도록 개선

--- src/js/profile_modify.js
+++ src/js/profile_modify.js
@@ -1,56 +1,47 @@
 // 이전 페이지로 이동
 const btnBack = document.querySelector('.btn-back');
-
 btnBack.addEventListener('click', () => {
     window.history.back();
 });
 
 
-// 유효성 검사
-const formUserProfile = document.querySelector('.form-user-profile');
+//유효성 검사 
+const nameInput = document.getElementById('username');
+const idInput = document.getElementById('accountname');
 const btnSave = document.querySelector('.btn-save');
-let btnSaveFlag = false;
+let nameFlag = false;
+let idFlag = false;
 
-// form 에 change가 일어났을 때, 유효성 검사 후 저장버튼 활성화
-formUserProfile.addEventListener('change', () => {
-    infoModify_check();
-    btnAttrChange(); 
+nameInput.addEventListener('keyup', function() {
+    const RegExp1 = /^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]{2,10}$/;
+    if (!RegExp1.test(nameInput.value)) {
+        nameInput.focus();
+        nameFlag = false;
+    } else {
+        nameFlag = true;
+    }
+    btnAttrChange()
 });
 
-function infoModify_check() {
-    const userName = document.querySelector('#username');
-    const accountName = document.querySelector('#accountname');
-
-    const RegExp1 = /^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]{2,10}$/;
-    const RegExp2 = /^[a-z0-9_,]+$/; 
-
-    // username 유효성 검사
-    if (!RegExp1.test(userName.value)) {
-        userName.focus();
-        btnSaveFlag = false;
-        return false;
+idInput.addEventListener('keyup', function() {
+    const RegExp2 = /^[a-z0-9_,]+$/;
+    if (!RegExp2.test(idInput.value)) {
+        idInput.focus();
+        idFlag = false;
+    } else {
+        idFlag = true;
     }
-
-    //accountname 유효성 검사
-    if (!RegExp2.test(accountName.value)) {
-        accountName.focus();
-        btnSaveFlag = false;
-        return false;
-    }
-
-    btnSaveFlag = true;
-}
-
+    btnAttrChange()
+});
 
 // 저장 버튼 활성화 및 비활성화
 function btnAttrChange() {
-    if (btnSaveFlag) {
+    if (nameFlag && idFlag) {
         btnSave.setAttribute('type', 'submit');
     } else {
         btnSave.setAttribute('type', 'button');
     }
 }
-
 
 //프로필 사진 변경
 const imgInput = document.querySelector('#input-img');
@@ -59,9 +50,8 @@ imgInput.addEventListener('change', (e) => {
     let image = e.target.files[0]; //파일 선택
     const reader = new FileReader();
     reader.readAsDataURL(image); // 파일을 읽는 메서드
-    reader.addEventListener('load', ()=>{
+    reader.addEventListener('load', () => {
         console.log(reader.result);
-        imgLabel.style = `background-image : url(${reader.result}); background-position: center; background-size : cover`
-    })
-    
-})
+        imgLabel.style = `background-image : url(${reader.result}); background-position: center; background-size : cover`;
+    });
+});
