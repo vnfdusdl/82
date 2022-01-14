a191a8e596c973c9479df69ee0e62746378ee217
choyeon <rlachdus2@naver.com>
2022-01-14 18:34:10 +0900

    ✨ 프로필 수정 API 연동

--- src/js/profile_modify.js
+++ src/js/profile_modify.js
@@ -4,15 +4,14 @@ btnBack.addEventListener('click', () => {
     window.history.back();
 });
 
-
-//유효성 검사 
+//유효성 검사
 const nameInput = document.getElementById('username');
 const idInput = document.getElementById('accountname');
 const btnSave = document.querySelector('.btn-save');
 let nameFlag = false;
 let idFlag = false;
 
-nameInput.addEventListener('keyup', function() {
+nameInput.addEventListener('keyup', function () {
     const RegExp1 = /^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]{2,10}$/;
     if (!RegExp1.test(nameInput.value)) {
         nameInput.focus();
@@ -20,10 +19,10 @@ nameInput.addEventListener('keyup', function() {
     } else {
         nameFlag = true;
     }
-    btnAttrChange()
+    btnAttrChange();
 });
 
-idInput.addEventListener('keyup', function() {
+idInput.addEventListener('keyup', function () {
     const RegExp2 = /^[a-z0-9_,]+$/;
     if (!RegExp2.test(idInput.value)) {
         idInput.focus();
@@ -31,7 +30,7 @@ idInput.addEventListener('keyup', function() {
     } else {
         idFlag = true;
     }
-    btnAttrChange()
+    btnAttrChange();
 });
 
 // 저장 버튼 활성화 및 비활성화
@@ -43,37 +42,64 @@ function btnAttrChange() {
     }
 }
 
-// 프로필 사진 변경 
-document.querySelector("#input-img").addEventListener("change",profileImage)
-let imgPreview =  document.getElementById('label-img');
+// 프로필 사진 변경
+document.querySelector('#input-img').addEventListener('change', profileImage);
+let imgPreview = document.getElementById('label-img');
 
 async function profileImage(e) {
     const files = e.target.files;
-    const result = await imageUpload(files); 
-    imgPreview.style.backgroundImage = `url(http://146.56.183.55:5050/${result})`
-    imgPreview.style.backgroundPosition =`center`
-    imgPreview.style.backgroundSize = `cover`
+    const result = await imageUpload(files);
+    imgPreview.style.backgroundImage = `url(http://146.56.183.55:5050/${result})`;
+    imgPreview.style.backgroundPosition = `center`;
+    imgPreview.style.backgroundSize = `cover`;
 }
 
-async function imageUpload(files){
+async function imageUpload(files) {
     const formData = new FormData();
-    formData.append("image", files[0]);
-    
+    formData.append('image', files[0]);
+
     const res = await fetch(`http://146.56.183.55:5050/image/uploadfile`, {
-        method: "POST",
-        body : formData
-    }) 
-    
-    const data = await res.json()
-    const imgFileName = data["filename"];
-    return imgFileName
+        method: 'POST',
+        body: formData,
+    });
+
+    const data = await res.json();
+    const imgFileName = data['filename'];
+    return imgFileName;
 }
 
+//작성된 내용을 서버에 전송
+btnSave.addEventListener('click', formSubmit);
 
-
-
-//작성한 form을 서버에 전송
-
-
-
-
+async function formSubmit() {
+    const userName = nameInput.value;
+    const accountName = idInput.value;
+    const intro = document.querySelector('#intro').value;
+    const imageUrl = imgPreview.style.backgroundImage;
+    const token = localStorage.getItem('Token');
+    try {
+        const res = await fetch(`http://146.56.183.55:5050/user`, {
+            method: 'PUT',
+            headers: {
+                Authorization: `Bearer ${token}`,
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify({
+                user: {
+                    username: userName,
+                    accountname: accountName,
+                    intro: intro,
+                    image: imageUrl,
+                },
+            }),
+        });
+        const json = await res.json();
+        if (res.status == 200) {
+            location.href = './myprofile.html';
+        } else {
+            console.log(json);
+        }
+    } catch (err) {
+        alert(err);
+    }
+}
