daf2bf1c7c6499b94b9de89496906db384809ed6
useon <yoosk4986@naver.com>
2022-01-15 18:31:39 +0900

    💄 home UI 수정
    🐛 heartCount 연동 오류 해결
    
    ✨ 사진 여러 장인 경우 첫번째 사진만 표시하는 기능
    
    🔥 불필요한 파일삭제
    
    ✨ 팔로잉 수에 따른 첫 화면 구성
    
    ✨ 피드 옵션버튼 클릭시 모달창 기능
    
    💄 home UI 수정
    
    💄 피드에서 옵션 모달창 스타일 수정
    
    🔥 home 불필요한 코드 정리
    
    ✨ 검색키워드에 따른 검색 기능
    
    💄 search 뒤로가기 경로 및 UI 수정

--- src/js/search.js
+++ src/js/search.js
@@ -1,6 +1,12 @@
-const searchedUser = document.querySelector(".input_search").value;
+const input = document.querySelector(".input_search");
+const searchedUser = input.value;
 // const searchedUser = "b";
 const userListContainer = document.querySelector(".list_searchResult");
+
+// input값 저장하기
+function inputData() {
+  console.log(searchedUser)
+}
 
 async function getSearchedUserProfile() {
   const url = "http://146.56.183.55:5050";
@@ -13,21 +19,40 @@ async function getSearchedUserProfile() {
     },
   });
   const json = await res.json();
-  //forEach문으로 받아온 데이터 전부 살펴보면서 그려주는 부분
-  json.forEach((user) => {
-    const accountname = user.accountname;
-    const image = user.image;
-    const username = user.username;
-    userListContainer.innerHTML += `
-      <li class="list_user">
-        <img src="${image}" alt="" />
-        <div class="user_wrap">
-          <p>${username}</p>
-          <span>@ ${accountname}</span>
-        </div>
-      </li>             
-      `;
-  });
+
+  input.addEventListener('keypress', (event) => {
+    console.log(input.value);
+    const userId = input.value;
+    if (event.keyCode === 13) {
+      matchUserId(userId, json);
+      console.log('엔터');
+      input.value = '';
+      input.focus();
+    }
+  })
+
+}
+//일치하는 데이터를 그려주는 부분
+function matchUserId(userId, json) {
+  json.forEach(user => {
+    if (user.accountname === userId) {
+      console.log(`완료 : ${user.accountname}`);
+      const accountname = user.accountname;
+      const image = user.image;
+      const username = user.username;
+      userListContainer.innerHTML = ''; //약간의 의문?.?
+      userListContainer.innerHTML += `
+        <li class="list_user">
+          <img src="${image}" alt="" />
+          <div class="user_wrap">
+            <p>${username}</p>
+            <span>@ ${accountname}</span>
+          </div>
+        </li>             
+        `;
+    }
+  })
+
 }
 
 userListContainer.addEventListener("click", (e) => {
@@ -58,5 +83,6 @@ userListContainer.addEventListener("click", (e) => {
     localStorage.setItem("searchedUserAccountname", accountname);
     location.href = "./yourprofile.html";
   }
+
 });
 getSearchedUserProfile();
