267bb8dbca786dcc9210db68a436142558a766e8
choyeon <rlachdus2@naver.com>
2022-01-15 17:59:22 +0900

    ✨ 팔로잉 리스트 API 연동 팔로워의 버튼에 나의 맞팔로우 정보가 표시됨

--- src/js/followers.js
+++ src/js/followers.js
@@ -2,6 +2,7 @@
 async function getFollowerData() {
     const token = localStorage.getItem('Token');
     const accountName = localStorage.getItem('Accountname');
+    //나의 팔로우 리스트
     const followingRes = await fetch(
         `http://146.56.183.55:5050/profile/${accountName}/following`,
         {
@@ -11,54 +12,67 @@ async function getFollowerData() {
                 'Content-type': 'application/json',
             },
         }
-    );
-    // const followingjson = await followingRes.json();
-    // followingjson.map((following) => {
-    //     const followingAccountArray = [];
-    //     followingAccountArray.psuh(following.accountname);
-    // })
+        );
+        //내가 팔로우 한 유저의 id를 담음
+        const followingjson = await followingRes.json();
+        const followingIdArray = [];
+        followingjson.map((following) => {
+            followingIdArray.push(following._id);
+        })
+        
+        //나의 팔로워 리스트
+        const followerRes = await fetch(
+            `http://146.56.183.55:5050/profile/${accountName}/follower`,
+            {
+                method: 'GET',
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    'Content-type': 'application/json',
+                },
+            }
+            );
+            
+            const followerjson = await followerRes.json();
+            // console.log(followerjson);
+            followerjson.map((follower) => {
+                const followerId = follower._id;
+                const userName = follower.username;
+                const intro = follower.intro;
+                const image = follower.image;
+                const followerAccountName = follower.accountname;
+                
+                
+                document.querySelector('.list-followers').innerHTML += `
+                <li>
+                <a href="" class="cont_user">
+                <img src="${image}"  class="img_profile" />
+                <div>
+                <strong>${userName}</strong>
+                <small class="txt_preview txt_ellipsis">${intro}</small>
+                </div>
+                </a>
+                <button id="${followerAccountName}" class="btn-follow" ></button>
+                </li>
+                `;
 
-    const followerRes = await fetch(
-        `http://146.56.183.55:5050/profile/${accountName}/follower`,
-        {
-            method: 'GET',
-            headers: {
-                Authorization: `Bearer ${token}`,
-                'Content-type': 'application/json',
-            },
-        }
-    );
-
-    const followerjson = await followerRes.json();
-    // console.log(followerjson);
-    followerjson.map((follower) => {
-        const userName = follower.username;
-        const intro = follower.intro;
-        const image = follower.image;
-        const followerAccountName = follower.accountname;
-        // console.log(followerAccountName);
-        let followersList = (document.querySelector(
-            '.list-followers'
-        ).innerHTML += `
-        <li>
-        <a href="" class="cont_user">
-        <img src="${image}"  class="img_profile" />
-        <div>
-        <strong>${userName}</strong>
-        <small class="txt_preview txt_ellipsis">${intro}</small>
-        </div>
-        </a>
-        <button id="${followerAccountName}" class="btn-follow" ></button>
-        </li>
-        `);
-    });
-
-    //팔로우, 언팔로우 기능
-    const followBtns = document.querySelectorAll('.btn-follow');
-    for (const followBtn of followBtns) {
-        //follow버튼을 클릭했을 때,
-        followBtn.addEventListener('click', () => {
-            // followBtn.classList.toggle('following');
+                // 맞팔 유무 띄워주기
+                const followFlag = followingIdArray.find(x => x === followerId);
+                console.log(!!followFlag); //true랑 false로 나옴
+                const followBtn = document.getElementById(followerAccountName)
+                console.log(followBtn)
+                if(!!followFlag) {
+                    followBtn.classList.add('following');
+                    followBtn.textContent = '취소';
+                } else {
+                    followBtn.textContent = '팔로우';
+                }
+            });
+            
+            //팔로우, 언팔로우 기능
+            const followBtns = document.querySelectorAll('.btn-follow');
+            for (const followBtn of followBtns) {
+                //follow버튼을 클릭했을 때,
+                followBtn.addEventListener('click', () => {
             const accountName = followBtn.getAttribute('id');
 
             //following 중이라면
@@ -78,7 +92,6 @@ async function getFollowerData() {
                     );
                     const json = await res.json();
                     console.log(json);
-                    // followBtn.classList.remove('following');
                     followBtn.textContent = '팔로우';
                 }
                 UnFollow();
